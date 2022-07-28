8558efb03b1bb3a3e88add9585f1c2e343a15e32
pinkman <1095009456@qq.com>
2022-07-28 21:12:47 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入全局vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// 引入基准字号
+import 'amfe-flexible'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
